8ed5064de72ffe5bfb19bdac199faa3d3eeb0159
Mohit Musaddi <96mohitm@gmail.com>
2019-11-11 20:29:23 +0530

    commented and more logical code added for kmp algorithm string matching.

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -5,6 +5,42 @@
 first we make lps of the pattern that we want to fing in the string
 */
 using namespace std;
+// Fills lps[] for given patttern pat[0..M-1] 
+void computeLPSArray(char* pat, int M, int* lps) 
+{ 
+    // length of the previous longest prefix suffix 
+    int len = 0; 
+  
+    lps[0] = 0; // lps[0] is always 0 
+  
+    // the loop calculates lps[i] for i = 1 to M-1 
+    int i = 1; 
+    while (i < M) { 
+        if (pat[i] == pat[len]) { 
+            len++; 
+            lps[i] = len; 
+            i++; 
+        } 
+        else // (pat[i] != pat[len]) 
+        { 
+            // This is tricky. Consider the example. 
+            // AAACAAAA and i = 7. The idea is similar 
+            // to search step. 
+            if (len != 0) { 
+                len = lps[len - 1]; 
+  
+                // Also, note that we do not increment 
+                // i here 
+            } 
+            else // if (len == 0) 
+            { 
+                lps[i] = 0; 
+                i++; 
+            } 
+        } 
+    } 
+} 
+
 vector<int> prefix_function(string s){
 	int n = (int)s.length();
 	vector<int> pi(n);
